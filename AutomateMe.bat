bcf043942d72a4849c4d7d5fb39794b3ad39b705
charles Coulter <charlesjcoulter@gmail.com>
2016-03-05 14:35:48 -0500

    YAY

---
+++ AutomateMe.bat
@@ -0,0 +1,39 @@
+::@echo off
+
+:: This script will automate the packing of a pbo file
+:: IT will move the mission PBO to the mpmission folder that your specify as well as copy the file to your MPMissionCache
+
+SETLOCAL ENABLEDELAYEDEXPANSION
+	
+	::DEFINES
+		
+		Set PboConsoleExe= "C:\Program Files\PBO Manager v.1.4 beta\PBOConsole.exe"
+		Set PboFileName= "D:\Documents\Docs\Dev_ArmA3_Wasteland.Altis.pbo"
+		Set Source= "D:\Documents\Docs\ArmA3_Wasteland.Altis"
+		Set ServerMpMission= "D:\dev\MPMissions\"
+		Set MpMissionCache=  "C:\Users\CJ\AppData\Local\Arma 3\MPMissionsCache\"
+		Set TempMissionDir= "D:\Documents\Docs\Temp_Arma3_Wasteland.Altis"
+		
+
+	::Create GitTempDir
+	mkdir %TempMissionDir%
+	
+	::Copy Source
+	xcopy %Source% %TempMissionDir% /s /i
+	
+	::Pack into PBO
+	%PboConsoleExe% -pack %TempMissionDir% %PboFileName%
+
+	::Copy to Server MPMissions and MPMissionCache
+	copy %PboFileName% %ServerMpMission%
+	copy %PboFileName% %MpMissionCache%
+	
+	
+	::Clean 
+	del %PboFileName% /q
+	rmdir %TempMissionDir% /s /q
+	
+	
+		
+		
+		
